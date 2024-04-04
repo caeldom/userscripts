e21d707c0a1e01e263d4481fe0010d2bac1a1758
Dandandan Lim <caeldom@gmail.com>
2024-04-05 02:10:42 +1100

    Remove jquery, rewrite using mutation observer
    Jquery was failing and causing CSP error "This document requires 'TrustedHTML' assignment." due to using innerHTML which is not allowed under updated strict CSP
    See https://github.com/violentmonkey/violentmonkey/issues/1873
    Replace jquery require with cash
    Replace jquery initialize with mutation observer

--- gmail-highlight-starred.user.js
+++ gmail-highlight-starred.user.js
@@ -1,35 +1,48 @@
 // ==UserScript==
 // @name         Gmail Highlight Starred
-// @version      1.0
+// @version      2.0
 // @description  Highlight emails when they are starred
 // @author       Dan Lim
 // @match        https://mail.google.com/mail/u/*
-// @require      https://code.jquery.com/jquery-3.6.0.min.js
-// @require      https://cdn.jsdelivr.net/npm/jquery.initialize@1.3.0/jquery.initialize.min.js
+// @require      https://cdn.jsdelivr.net/npm/cash-dom/dist/cash.min.js
 // @icon         https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico
+// @updateURL    https://github.com/caeldom/userscripts/raw/main/gmail-highlight-starred.user.js
+// @downloadURL  https://github.com/caeldom/userscripts/raw/main/gmail-highlight-starred.user.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // highlight yellow starred
-    $.initialize(
-        'span[aria-label="Starred"]',
-        function () { $(this).closest('tr').css('backgroundColor', 'rgb(98,82,40,0.6)') }
-    );
+    // initialise a mutation observer on the inbox container
+    (function initObserver() {
+        let MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+        let inboxContainer = $('div.AO')[0];
 
-    // highlight red starred
-    $.initialize(
-        'span[aria-label="Starred with \\"red-star\\""]',
-        function () { $(this).closest('tr').css('backgroundColor', 'rgb(219,68,55,0.5)') }
-    );
-
-    // un-highlight
-    $.initialize(
-        'span[aria-label="Not starred"]',
-        function () {
-            $(this).closest('tr').removeAttr('style');
+        if (inboxContainer && MutationObserver) {
+            let mutationObserver = new MutationObserver((records, observer) => {
+                highlightStarred();
+                // only trigger once then reset observer to reduce redundant calls
+                observer.disconnect();
+                initObserver();
+            });
+            // check when aria label attribute on element changes to trigger
+            mutationObserver.observe(inboxContainer, { childList:true, subtree: true, attributes: true, attributeFilter: ['aria-label'] });
+        } else {
+            // wait until the element exists
+            setTimeout(initObserver, 300);
         }
-    );
+    })();
 
+    function highlightStarred() {
+        console.log("Highlighting starred emails");
+        // uses cash instead of jquery
+        // highlight yellow starred
+        $('span.T-KT.T-KT-Jp').closest('tr').css('backgroundColor', 'rgb(98,82,40,0.6)');
+
+        // highlight red starred
+        $('span.T-KT.xn').closest('tr').css('backgroundColor', 'rgb(219,68,55,0.5)');
+
+        // un-highlight not starred
+        $('span.T-KT.aXw').closest('tr').removeAttr('style');
+    }
 })();
